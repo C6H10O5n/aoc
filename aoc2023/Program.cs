326af631f924cd27fe84e255daa9b7687009cfcc
Robert O'Keefe <okeefern@gov.ns.ca>
2023-12-04 15:47:35 -0400

    added timer

--- aoc2023/Program.cs
+++ aoc2023/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace aoc2023_02
 {
@@ -6,23 +7,37 @@ namespace aoc2023_02
     {
         private static void Main(string[] args)
         {
+            var timer = new Stopwatch();
+
             Console.WriteLine("Advent of Code 2023:.....");
 
 
             Console.WriteLine($"\n\nDay 1 Problem:");
+            timer.Start();
             day1();
+            timer.Stop();
+            Console.WriteLine($"Elapsed Time: {timer.Elapsed.TotalSeconds} seconds");
 
 
             Console.WriteLine($"\n\nDay 2 Problem:");
+            timer.Start();
             day2();
+            timer.Stop();
+            Console.WriteLine($"Elapsed Time: {timer.Elapsed.TotalSeconds} seconds");
 
 
             Console.WriteLine($"\n\nDay 3 Problem:");
+            timer.Start();
             day3();
+            timer.Stop();
+            Console.WriteLine($"Elapsed Time: {timer.Elapsed.TotalSeconds} seconds");
 
 
             Console.WriteLine($"\n\nDay 4 Problem:");
+            timer.Start();
             day4();
+            timer.Stop();
+            Console.WriteLine($"Elapsed Time: {timer.Elapsed.TotalSeconds} seconds");
 
 
 
