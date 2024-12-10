8e5317997d453ebb671643bda3c1ab8709b773d3
Robert O'Keefe <okeefern@gov.ns.ca>
2024-12-10 09:05:44 -0400

    day9 done!!!

--- aoc2024/day09/day9.cs
+++ aoc2024/day09/day9.cs
@@ -15,7 +15,7 @@ namespace aoc2024
 
         static void day9()
         {           
-            //Console.WriteLine($"Answer1: {day9LogicPart1()}");
+            Console.WriteLine($"Answer1: {day9LogicPart1()}");
             Console.WriteLine($"Answer2: {day9LogicPart2()}");
         }
                 
@@ -23,8 +23,8 @@ namespace aoc2024
         static long day9LogicPart1()
         {
             //var fx = d9_data0[0].ToCharArray().Select(c => c - '0').ToList();
-            var fxd = d9_data0[0].ToCharArray().Where((c, i) => i % 2 == 0).Select(c => c - '0').ToList();
-            var fxs = d9_data0[0].ToCharArray().Where((c, i) => i % 2 != 0).Select(c => c - '0').ToList();
+            var fxd = d9_data[0].ToCharArray().Where((c, i) => i % 2 == 0).Select(c => c - '0').ToList();
+            var fxs = d9_data[0].ToCharArray().Where((c, i) => i % 2 != 0).Select(c => c - '0').ToList();
 
             var fxdSum = fxd.Sum();
             var fxdCnt = fxd.Count;
@@ -57,47 +57,49 @@ namespace aoc2024
 
         static long day9LogicPart2()
         {
-            //8564936405055 to high
+            //8564936405055 to high  --needed to reclaim space from moved numbers: added decrement to line 81 [fxs[i] += fxd[i];]
+            //6368144863485 to high -- need to stop searching foward: added [ && j<i] in line 82 if statement
+            //6362722604045
 
             //var fx = d9_data0[0].ToCharArray().Select(c => c - '0').ToList();
-            var fxd = d9_data0[0].ToCharArray().Where((c, i) => i % 2 == 0).Select(c => c - '0').ToList();
-            var fxs = d9_data0[0].ToCharArray().Where((c, i) => i % 2 != 0).Select(c => c - '0').ToList();
+            var fxd = d9_data[0].ToCharArray().Where((c, i) => i % 2 == 0).Select(c => c - '0').ToList();
+            var fxs = d9_data[0].ToCharArray().Where((c, i) => i % 2 != 0).Select(c => c - '0').ToList();
             fxs.Add(0);
 
 
             var rx = fxd.Select((x, i) => (x, i))
                     .Zip(fxs.Select((y) => y), (a, b) => Enumerable.Range(0, a.x).Select(_ => a.i).Concat(Enumerable.Range(0, b).Select(_ => -1))
                     .ToList()).ToList();
-                    //.SelectMany(x=>x); 
 
-            for (int i = fxd.Count-1; i >=0; i--)
+            //Console.WriteLine("Input String: "+d9_data0[0]);
+            for (int i = fxd.Count - 1; i >= 0; i--)
             {
+                //Console.WriteLine((new string(i.ToString()[0],fxd[i])+"     ").Substring(0,5)+string.Join("",rx.SelectMany(x=>x).Select(x=>x==-1?".":x==-2?"-":x.ToString())));
+
                 for (int j = 0; j < fxs.Count; j++)
                 {
-                    if (fxd[i] <= fxs[j])
+
+                    if (fxd[i] <= fxs[j] && j<i)
                     {
                         var sp = rx[j].Count() - fxs[j];
-                        for (int k =0; k < fxd[i]; k++)
+                        for (int k = 0; k < fxd[i]; k++)
                         {
-                            rx[j][k+sp] = i;
+                            rx[j][k + sp] = i;
                             rx[i][k] = -2;
                         }
                         fxs[j] -= fxd[i];
+                        fxs[i] += fxd[i];
                         break;
                     }
                 }
             }
 
-            return rx.SelectMany(x => x).Select((xd, xi) => (xd, xi)).Where(x=>x.xd>=0).Sum(x => (long)(x.xd*x.xi));
+            return rx.SelectMany(x => x).Select((xd, xi) => (xd, xi)).Where(x=>x.xd>0).Sum(x => (long)(x.xd*x.xi));
         }
 
         static string[] d9_data0 =
         """        
-        002333133121414131402
-        """.Split(Environment.NewLine);
-        static string[] d9_data01 =
-        """        
-        12345
+        2333133121414131402
         """.Split(Environment.NewLine);
 
     }
